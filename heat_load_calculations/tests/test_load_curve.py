ebc1c5a7fe2144146fe61e89a265f8e2a73b06a2
McMillan <cmcmilla@nrel.gov>
2020-02-19 10:40:46 -0500

    Updated to test all ind-size combos

--- heat_load_calculations/tests/test_load_curve.py
+++ heat_load_calculations/tests/test_load_curve.py
@@ -1,14 +1,24 @@
 import pytest
 import make_load_curve
 import numpy as np
+import pandas as pd
 
-def test_make_load_curve():
+# Create all combinations of naics and emp_size
+naics_emp = pd.read_parquet(
+    'c:/users/cmcmilla/solar-for-industry-process-heat/results/mfg_eu_temps_20191031_2322.parquet.gzip',
+    columns=['naics', 'Emp_Size']
+    )
+
+# naics_emp = naics_emp.drop_duplicates().values
+
+naics_emp = [(321219, 'n100_249')]
+@pytest.mark.parametrize("naics, emp_size", naics_emp)
+def test_make_load_curve(naics, emp_size):
 
     annual_mmbtu = 100
-
-    naics_og = 331110
-
-    emp_size = 'n1_4'
+    # naics_og = 331110
+    #
+    # emp_size = 'n1_4'
 
     hours = 'qpc'
 
@@ -17,7 +27,7 @@ def test_make_load_curve():
     lc = make_load_curve.load_curve()
 
     load_shape = lc.calc_load_shape(
-        naics_og, emp_size, enduse_turndown={'boiler': 4}, hours=hours,
+        naics, emp_size, enduse_turndown={'boiler': 4}, hours=hours,
         energy=energy
         )
 
