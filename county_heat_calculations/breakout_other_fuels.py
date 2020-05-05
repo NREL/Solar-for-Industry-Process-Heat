2bb710e1b865fda6a3e425dbd4d8723269666591
calmc <cmcmill@gmail.com>
2020-05-05 17:12:23 -0400

    Fuel breakout and emissions calcs

--- county_heat_calculations/breakout_other_fuels.py
+++ county_heat_calculations/breakout_other_fuels.py
@@ -1,13 +1,16 @@
 
+import pandas as pd
+import os
 
-class
+class Other_fuels:
 
     def __init__(self, year):
 
         self.year = year
 
-        formatted_files = ['bio'+str(2014)+'_formatted.csv',
-                           'byp'+str(2014)+'_formatted.csv']
+        formatted_files = ['bio'+str(self.year)+'_formatted.csv',
+                           'byp'+str(self.year)+'_formatted.csv',
+                           'table5_2_'+str(self.year)+'_formatted.csv']
 
         for f in formatted_files:
 
@@ -20,12 +23,73 @@ class
                 print(f, 'does not exist in /calculation_data/', '\n',
                       'Please create it')
 
+
+    @staticmethod
+    def map_GHGRP_fueltypes(energy_ghgrp, ft_file):
+        """
+        Map GHGRP fuel types to MECS fuel types, either using aggregated
+        (ft_file = MECS_FT_IPF.csv) or diaggregated fuel types (ft_file =
+        MECS_FT_byp.csv).
+        Returns energy_ghgrp dataframe with new fuel type column.
+        """
+
+        if ft_file == 'MECS_FT_IPF.csv':
+
+            new_ft_column = 'MECS_FT'
+
+        else:
+
+            new_ft_column = 'MECS_FT_byp'
+
+        ghgrp_mecs_ftxwalk = pd.read_csv('./calculation_data/'+ft_file)
+
+        # Pull out all fuel types used in GHGRP
+        ghgrp_fuels = pd.DataFrame(pd.concat(
+            [pd.Series(energy_ghgrp[c].dropna()) for c in ['FUEL_TYPE_OTHER',
+                                                           'FUEL_TYPE_BLEND',
+                                                           'FUEL_TYPE']],
+            axis=0, ignore_index=False
+            )).sort_index()
+
+        ghgrp_fuels.loc[:, 'index'] = ghgrp_fuels.index
+
+        ghgrp_fuels = pd.merge(
+            ghgrp_fuels, ghgrp_mecs_ftxwalk, left_on=0,
+            right_on='EPA_FUEL_TYPE', how='left')
+
+        ghgrp_fuels.set_index('index', inplace=True)
+
+        # Fill all remaining fuels as "Other"
+        ghgrp_fuels.fillna('Other', inplace=True)
+
+        energy_ghgrp = energy_ghgrp.join(ghgrp_fuels[new_ft_column], how='left')
+
+        return energy_ghgrp
+
+    def breakout_other(self):
+        """
+        Breakout MECS intensities into byproducts
+        """
+        #From byproducts Table3.5: Waste_oils_tars_waste_materials,
+        #Blast_furnace_coke_oven_gases, Waste_gas, Petroleum_coke
+        #From Wood and wood-related Table3.6: create sum of pulping liquor
+        # and total biomass for Biomass total.
         bio_table = pd.read_csv('./calculation_data/' + \
-                                'bio' + str(2014) +'_formatted.csv')
+                                'bio'+str(self.year)+'_formatted.csv')
 
         byp_table = pd.read_csv('./calculation_data/' +\
-                                'byp' + str(2014) + '_formatted.csv')
+                                'byp'+str(self.year)+'_formatted.csv')
 
+        eu_frac_nonGHGRP = pd.read_csv(
+            './calculation_data/eu_frac_nonGHGRP_' + str(2014) + '.csv',
+            index_col=['MECS_NAICS'],
+            usecols=['MECS_NAICS', 'CHP and/or Cogeneration Process',
+                     'Conventional Boiler Use', 'Process Heating', 'MECS_FT']
+            )
+
+        eu_frac_nonGHGRP = pd.DataFrame(
+            eu_frac_nonGHGRP[eu_frac_nonGHGRP.MECS_FT == 'Other']
+            )
 
         def format_biobyp_tables(df):
 
@@ -37,6 +101,10 @@ class
 
                     continue
 
+                elif c == 'naics':
+
+                    df[c] = df[c].astype('int')
+
                 else:
 
                     df[c] = df[c].astype('float32')
@@ -47,20 +115,73 @@ class
 
         byp_table = format_biobyp_tables(byp_table)
 
-        eu_table.set_index(['naics', 'end_use'], inplace=True)
+        # eu_table.set_index(['naics', 'end_use'], inplace=True)
 
-        adj_total = eu_table.xs(
-            'TOTAL FUEL CONSUMPTION', level=1
-            ).total.subtract(
-                    eu_table.xs('End Use Not Reported',
-                                level=1).total, level=1
-                    )
+        # adj_total = eu_table.xs(
+        #     'TOTAL FUEL CONSUMPTION', level=1
+        #     ).total.subtract(
+        #             eu_table.xs('End Use Not Reported',
+        #                         level=1).total, level=1
+        #             )
+        #
+        # adj_total.name = 'adj_total'
+        #
+        # eu_table.reset_index('end_use', inplace=True)
 
-        adj_total.name = 'adj_total'
+        byp_table_final = pd.merge(
+            byp_table.set_index(['naics', 'region']),
+            pd.DataFrame(
+                bio_table.set_index(['naics', 'region'])[
+                    ['pulp_liq', 'total_bio']
+                    ].sum(axis=1)
+                    ), left_index=True, right_index=True, how='left'
+            )
 
-        eu_table.reset_index('end_use', inplace=True)
+        byp_table_final.rename(
+            columns={0: 'Biomass', 'pet_coke': 'Petroleum_coke',
+                     'blast_furnace': 'Blast_furnace_coke_oven_gases',
+                     'waste_gas': 'Waste_gas',
+                     'waste_oils': 'Waste_oils_tars_waste_materials'},
+            inplace=True
+            )
 
-    def breakout_other(year):
-        """
+        byp_table_final.index.names = ['MECS_NAICS', 'MECS_REGION']
 
-        """
+        # Table 3.5 reports values for NAICS codes not provided in Table 3.6
+        missing_bio = byp_table_final[byp_table_final.Biomass.isna()]
+
+        missing_bio.Biomass.update(missing_bio.wood_chips)
+
+        byp_table_final.Biomass.update(missing_bio.Biomass)
+
+        # Remove Blast furnace/Coke oven gas values because these are
+        # captured by GHGRP data and non-integrated mills would not have
+        # blast furnace gas emissions.
+        byp_table_final.loc[:, 'Blast_furnace_coke_oven_gases'] = 0
+
+        # List of byproduct fuels to diaggregate Other fuel use by.
+        byp = ['Blast_furnace_coke_oven_gases', 'Waste_gas', 'Petroleum_coke',
+               'Waste_oils_tars_waste_materials', 'Biomass']
+
+        byp_table_final.total.update(byp_table_final[byp].sum(axis=1))
+
+        byp_table_final.update(byp_table_final[byp].divide(
+            byp_table_final.total, axis=0)
+            )
+
+        byp_table_final.fillna(0, inplace=True)
+
+        # Prep table for merge
+        byp_table_final = pd.melt(
+            byp_table_final[byp].reset_index(),
+            id_vars=['MECS_NAICS', 'MECS_REGION'], var_name='MECS_FT_byp'
+            )
+
+        byp_table_final = pd.merge(
+            byp_table_final[byp], eu_frac_nonGHGRP, left_index=True,
+            right_index=True, how='outer'
+            )
+
+        return byp_table_final
+
+        # Later, multiply this by eu_frac_nonGHGRP to get
--- county_heat_calculations/ghg.py
+++ county_heat_calculations/ghg.py
@@ -1,6 +1,70 @@
 
 
-class GHGs:
+class Emissions:
+
+    def __init__(self, years):
+
+        # Define as a range or list
+        self.years = years
+
+        ghgrp_emissions = pd.read_parquet(
+            '../results/ghgrp_energy_20200505-1117.parquet', engine='pyarrow',
+            columns=['FACILITY_ID', 'REPORTING_YEAR', 'FUEL_TYPE',
+                     'FUEL_TYPE_OTHER', 'FUEL_TYPE_BLEND', 'MTCO2e_TOTAL',
+                     'MMBtu_TOTAL']
+            )
+
+        ghgrp_emissions = pd.DataFrame(
+            ghgrp_emissions[ghgrp_emissions.REPORTING_YEAR.isin(self.years)
+            )
+
+        # Map aggregated fuel types to GHGRP fuel types
+        ghgrp_emissions = map_GHGRP_fueltypes(ghgrp_emissions,
+                                              'MECS_FT_IPF.csv')
+
+        # Map disaggregated fuel types to GHGRP fuel types
+        ghgrp_emissions = map_GHGRP_fueltypes(ghgrp_emissions,
+                                                   'MECS_FT_byp.csv')
+
+        # Replace Biomass emissions with zero value
+        ghgrp_emissions.loc[ghgrp_emissions.MECS_FT_byp == 'Biomass'),
+                            'MTCO2e_TOTAL'
+                            ] = 0
+
+        # Sum emissions (MTCO2e) for specified year(s)
+        self.ghgrp_ffc_emissions = ghgrp_emissions.groupby(
+                ['FACILITY_ID', 'REPORTING_YEAR', 'MECS_FT', 'MECS_FT_byp']
+                ).MTCO2e_TOTAL.sum().dropna()
+
+        # Calculate CO2e intensity (MTCO2e/MMBtu)
+        self.ghgrp_CO2e_intensity = self.ghgrp_ffc_emissions.divide(
+                ghgrp_emissions.groupby(
+                    ['FACILITY_ID', 'REPORTING_YEAR', 'MECS_FT', 'MECS_FT_byp']
+                    ).MMBtu_TOTAL.sum().dropna()
+                )
+
+    def calc_ghgrp_fuel_intensity(self):
+        """
+
+        """
+
+        ghgrp_fuel_mix = self.ghgrp_emissions.groupby(
+            ['FACILITY_ID', 'REPORTING_YEAR', 'MECS_FT', 'MECS_FT_byp']
+            ).MTCO2e_TOTAL.sum().divide(
+                self.ghgrp_emissions.groupby(
+                    ['FACILITY_ID', 'REPORTING_YEAR', 'MECS_FT', 'MECS_FT_byp']
+                    ).MMBtu_TOTAL.sum()
+                )
+
+        # Use this fuel intensity for GHGRP data (linked by FACILITY_ID) after
+        # corrections for county and NAICS have been made.
+        return energy_ghgrp
+
+    def breakout_other(year):
+        """
+
+        """
+
 
     def calc_fuel_intensity():
         """
