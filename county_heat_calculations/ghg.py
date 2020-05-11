8dbc6080255136a60da6a14248ed87316cf6febc
calmc <cmcmill@gmail.com>
2020-05-10 21:57:29 -0400

    More GHG edits

--- county_heat_calculations/ghg.py
+++ county_heat_calculations/ghg.py
@@ -1,14 +1,22 @@
 
+import pandas as pd
+import calc_elect_ghgs
+import breakout_other_fuels
+import numpy as np
 
 class Emissions:
 
     def __init__(self, years):
 
         # Define as a range or list
+        if (type(years) != list) | (type(years)!=range):
+
+            years = [years]
+
         self.years = years
 
         # Define global warming potentials (100-year IPCC FAR)
-        self.gwp = {'CH4': 25, 'N2O': 298}
+        gwp = {'CH4': 25, 'N2O': 298}
 
         # Define standard emission factors (from EPA) for MECS Fuel types
         self.std_efs = pd.DataFrame(
@@ -34,7 +42,6 @@ class Emissions:
             self.std_efs.gCH4_per_mmbtu.multiply(gwp['CH4']/1000) +\
                 self.std_efs.gN2O_per_mmbtu.multiply(gwp['N2O']/1000))/1000
 
-
         ghgrp_emissions = pd.read_parquet(
             '../results/ghgrp_energy_20200505-1117.parquet', engine='pyarrow',
             columns=['FACILITY_ID', 'REPORTING_YEAR', 'FUEL_TYPE',
@@ -43,19 +50,25 @@ class Emissions:
             )
 
         ghgrp_emissions = pd.DataFrame(
-            ghgrp_emissions[ghgrp_emissions.REPORTING_YEAR.isin(years)
+            ghgrp_emissions[ghgrp_emissions.REPORTING_YEAR.isin(years)]
             )
 
-        # Map aggregated fuel types to GHGRP fuel types
-        ghgrp_emissions = map_GHGRP_fueltypes(ghgrp_emissions,
-                                              'MECS_FT_IPF.csv')
+        of = breakout_other_fuels.Other_fuels(2014)
 
-        # Map disaggregated fuel types to GHGRP fuel types
-        ghgrp_emissions = map_GHGRP_fueltypes(ghgrp_emissions,
-                                                   'MECS_FT_byp.csv')
+        def map_byproducts_ghgrp(ghgrp_data):
+
+            # Map aggregated fuel types to GHGRP fuel types
+            ghgrp_data = of.map_GHGRP_fueltypes(ghgrp_data, 'MECS_FT_IPF.csv')
+
+            # Map disaggregated fuel types to GHGRP fuel type
+            ghgrp_data = of.map_GHGRP_fueltypes(ghgrp_data, 'MECS_FT_byp.csv')
+
+            return ghgrp_data
+
+        ghgrp_emissions = map_byproducts_ghgrp(ghgrp_emissions)
 
         # Replace Biomass emissions with zero value
-        ghgrp_emissions.loc[ghgrp_emissions.MECS_FT_byp == 'Biomass'),
+        ghgrp_emissions.loc[ghgrp_emissions.MECS_FT_byp == 'Biomass',
                             'MTCO2e_TOTAL'
                             ] = 0
 
@@ -71,28 +84,81 @@ class Emissions:
                     ).MMBtu_TOTAL.sum().dropna()
                 )
 
-    def calc_ghgrp_fuel_intensity(self):
+        self.ghgrp_CO2e_intensity.name = 'MMTCO2e_per_MMBtu'
+
+        self.ghgrp_CO2e_intensity = pd.DataFrame(self.ghgrp_CO2e_intensity)
+
+        # Import end-use breakdowns
+        self.eufrac_ghgrp = \
+            pd.read_csv('./calculation_data/eu_frac_GHGRP_2014.csv',
+                        usecols=['MECS_NAICS', 'MECS_FT',
+                                 'CHP and/or Cogeneration Process',
+                                 'Conventional Boiler Use', 'Process Heating'])
+
+        self.eufrac_nonghgrp = \
+            pd.read_csv('./calculation_data/eu_frac_nonGHGRP_2014.csv',
+                        usecols=['MECS_NAICS', 'MECS_FT',
+                                 'CHP and/or Cogeneration Process',
+                                 'Conventional Boiler Use', 'Process Heating'])
+
+    def calc_ghgrp_fuel_intensity(self, energy_ghgrp_y):
+        """
+        NAICS codes of reported GHGRP data may be corrected based on Census
+        County Business Patterns data. Final GHGRP GHG intensity is based on
+        these corrected NAICS codes.
+
+        GHG intensity is calculated by county, NAICS, and MECS_FT_byp.
         """
 
-        """
+        #Drop non-manufacturing industries (MECS_NAICS == 0)
+        energy_ghgrp_y = energy_ghgrp_y[energy_ghgrp_y.MECS_NAICS !=0]
 
-        ghgrp_fuel_mix = self.ghgrp_emissions.groupby(
-            ['FACILITY_ID', 'REPORTING_YEAR', 'MECS_FT', 'MECS_FT_byp']
-            ).MTCO2e_TOTAL.sum().divide(
-                self.ghgrp_emissions.groupby(
-                    ['FACILITY_ID', 'REPORTING_YEAR', 'MECS_FT', 'MECS_FT_byp']
-                    ).MMBtu_TOTAL.sum()
+        energy_ghgp_y = map_byproducts_ghgrp(energy_ghgp_y)
+
+        energy_ghgrp_y = energy_ghgrp_y.groupby(
+                ['FACILITY_ID','MECS_Region', 'COUNTY_FIPS',
+                 'PRIMARY_NAICS_CODE','MECS_NAICS','MECS_FT', 'MECS_FT_byp'],
+                as_index=False
+                ).MMBtu_TOTAL.sum()
+
+        energy_ghgrp_y['COUNTY_FIPS'] = energy_ghgrp_y.COUNTY_FIPS.astype(int)
+
+        energy_ghgrp_y.rename(columns={'PRIMARY_NAICS_CODE':'naics'},
+                                       inplace=True)
+
+        final_ghgrp_CO2e_intensity = energy_ghgrp_y.set_index(
+            ['REPORTING_YEAR','FACILITY_ID', 'MECS_FT_byp']
+            ).MMBtu_TOTAL.multiply(
+                self.ghgrp_CO2_intensity.reset_index(
+                    'MECS_FT'
+                    ).MMTCO2e_per_MMBtu
+                )
+
+        final_ghgrp_CO2e_intensity.rename(
+            columns={'MMBtu_TOTAL':'MMTCO2e_per_MMBtu'}, inplace=True
+            )
+
+        final_ghgrp_CO2e_intensity = final_ghgrp_CO2e_intensity.join(
+            energy_ghgrp_y.set_index(
+                ['REPORTING_YEAR','FACILITY_ID', 'MECS_FT_byp']
+                )['COUNTY_FIPS', 'naics'], how='left'
+            )
+
+        final_ghgrp_CO2e_intensity = final_ghgrp_CO2e_intensity.reset_index().groupby(
+            ['REPORTING_YEAR', 'COUNTY_FIPS', 'naics', 'MECS_FT',
+             'MECS_FT_byp']
+             ).MMTCO2e_per_MMBtu.mean()
+
+        # Save for first time
+        if not os.path.isfile('./calculation_data/ghgrp_CO2e_intensity.csv'):
+
+            final_ghgrp_CO2e_intensity.to_csv(
+                './calculation_data/ghgrp_CO2_intensity.csv', index_col=False
                 )
 
         # Use this fuel intensity for GHGRP data (linked by FACILITY_ID) after
         # corrections for county and NAICS have been made.
         return energy_ghgrp
-
-    def breakout_other(year):
-        """
-
-        """
-
 
 
     def calc_fuel_intensity():
@@ -110,11 +176,3 @@ class Emissions:
         #https://www.epa.gov/sites/production/files/2020-04/documents/ghg-emission-factors-hub.pdf
         # Assumes steam generated with nat gas at 80% efficiency
 # MTCO2e/MMBtu
-
-    def calc_elect_intensity(self, county):
-        """
-        GHG intensity of grid electricity (MMTCO2e/MMBtu), calculated from
-        EPA eGrid.
-        """
-
-        
