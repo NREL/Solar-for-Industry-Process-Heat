c1f3c4b20e7ccea7c30980d5519bac142d443767
carriescho <47186876+carriescho@users.noreply.github.com>
2020-05-19 10:32:17 -0500

    Add files via upload

---
+++ tech_opportunity_analysis/Elec_boiler.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import End_use_eff
+
+class electric_boiler:
+
+    def __init__(self):
+        
+        
+            
+        #self.boiler_eu = mfg_energy[mfg_energy['End_use'] == "Conventional Boiler Use"].copy()
+
+        
+        #self.boiler_efficiency = {'Natural_gas': 0.75, 'LPG_NGL': 0.82, 'Diesel': 0.83,
+         #                  'Coal': 0.81, 'Residual_fuel_oil': 0.83, 'Coke_and_breeze': 0.70,
+         #                  'Other': 0.70 }
+                            #efficiency values based on Walker, M. et al. 2013. and IEA report
+        
+        
+        self.eboiler_max_cap = 190 #in MMBtu/hr
+        
+        self.eboiler_factor = 1 #represents number of e-boilers that could replace conventional
+        
+        #percent of installed boiler capacity <= max eboiler capacity 
+        self.percent_cap_replace = {'311': 0.18614*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.2388,
+                                    '312': 0.18614*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.2388,
+                                    '313': 0.25212*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.52453,
+                                    '314': 0.25212*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.52453,
+                                    '315': 0.25212*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.52453,
+                                    '316': 0.25212*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.52453,
+                                    '321': 0.22585*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.43429,
+                                    '322': -5*10**(-7)*(self.eboiler_max_cap*self.eboiler_factor)**2 + \
+                                            0.0014*(self.eboiler_max_cap*self.eboiler_factor)+0.0409,
+                                    '324': -9*10**(-7)*(self.eboiler_max_cap*self.eboiler_factor)**2 + \
+                                            0.002*(np.log(self.eboiler_max_cap*self.eboiler_factor))-0.04658,
+                                    '325': 0.21192*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.49737,
+                                    '326': 0.25212*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.52453,
+                                    '327': 0.25212*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.52453,
+                                    '331': 0.19494*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.45473,
+                                    '332': 0.25212*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.52453,
+                                    '333': 0.20511*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.3591,
+                                    '334': 0.25212*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.52453,
+                                    '335': 0.25212*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.52453,
+                                    '336': 0.20511*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.3591,
+                                    '337': 0.25212*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.52453,
+                                    '339': 0.25212*np.log(self.eboiler_max_cap*self.eboiler_factor)-0.52453
+                                   }
+                                    # equations based on e-boiler technical potential analysis
+       
+                                    
+    def calc_eboiler_process_energy(self, mfg_energy):    
+         
+        boiler_energy = End_use_eff.end_use_efficiency(mfg_energy).boiler_eu
+        boiler_eff = End_use_eff.end_use_efficiency(mfg_energy).boiler_efficiency
+        
+        boiler_energy.loc[:,'naics_sub'] = boiler_energy['naics'].astype(str).str[:3]
+        
+        #calculating proc energy based on efficiency only, need to ADD limit of capacity
+        boiler_energy.loc[:,'proc_MMBtu'] = boiler_energy.apply(
+            lambda x: boiler_eff[x['MECS_FT']] * self.percent_cap_replace[x['naics_sub']] * x['MMBtu'], axis=1 )
+        
+        return boiler_energy
+
+
+# In[ ]:
+
+
+
+
---
+++ tech_opportunity_analysis/End_use_eff.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+class end_use_efficiency:
+
+    def __init__(self,mfg_energy):
+        
+        
+        self.boiler_efficiency = {'Natural_gas': 0.75, 'LPG_NGL': 0.82, 'Diesel': 0.83,
+                           'Coal': 0.81, 'Residual_fuel_oil': 0.83, 'Coke_and_breeze': 0.70,
+                           'Other': 0.70 }
+                            #efficiency values based on Walker, M. et al. 2013. and IEA report
+        
+         
+        self.boiler_eu = mfg_energy.loc[mfg_energy['End_use'] == "Conventional Boiler Use"].copy()
+        
+        
+        
+        self.chp_efficiency = {'Boiler/Steam Turbine': 0.735,'Gas Turbine': 0.3942 }
+                            #efficiency values based on DOE CHP fact sheets, average values among diff capacity sizes
+        
+         
+        self.chp_eu = mfg_energy.loc[mfg_energy['End_use'] == "CHP and/or Cogeneration Process"].copy()
+        
+    def chp_pm_frac():
+    
+        #chp-mapped-to-county database for determining prime mover breakdown within county
+        chp_cty = pd.read_csv(
+        r'C:\Users\Carrie Schoeneberger\Gitlocal\Solar-for-Industry-Process-Heat\CHP\CHP with Counties_2012_NAICS_updated.csv')
+
+        #consider only steam turbines and gas turbines, which make up 95% of capacity
+        chp_cty = chp_cty.loc[(chp_cty['Prime Mover']=='Boiler/Steam Turbine') | 
+                              (chp_cty['Prime Mover']=='Combined Cycle') |
+                              (chp_cty['Prime Mover']=='Combustion Turbine')].copy()
+
+        chp_cty_pm = chp_cty.replace({'Prime Mover': 
+                                  {'Combined Cycle': 'Gas Turbine', 'Combustion Turbine': 'Gas Turbine'}})
+        
+        
+        #determine prime mover breakdown, by capacity, within each county
+        
+        chp_pm_county = chp_cty_pm.groupby(
+            ['FIPS County','Prime Mover'])['Capacity (kW)'].sum()/chp_cty_pm.groupby(['FIPS County'])['Capacity (kW)'].sum()
+
+        chp_county_frac = chp_pm_county.reset_index()
+        
+        
+        #list of all counties with chp mapped
+        chp_county_list = chp_cty_pm['FIPS County'].unique()
+        
+        #list of counties with steam turbine chp only, gas turbine only, and both
+        county_list_st = chp_county_frac.loc[(
+            chp_county_frac['Prime Mover']=='Boiler/Steam Turbine') & (chp_county_frac['Capacity (kW)']==1)]
+        
+
+        county_list_gas = chp_county_frac.loc[(
+            chp_county_frac['Prime Mover']=='Gas Turbine') & (chp_county_frac['Capacity (kW)']==1)]
+
+
+        county_list_both = chp_county_frac.loc[(chp_county_frac['Capacity (kW)']<1)]
+        
+        
+        county_chp_st = county_list_both[county_list_both['Prime Mover']=="Boiler/Steam Turbine"]
+        county_chp_st_dict = county_chp_st.loc[
+            :,('FIPS County', 'Capacity (kW)')].set_index('FIPS County')['Capacity (kW)'].to_dict()
+
+
+        county_chp_gas = county_list_both[county_list_both['Prime Mover']=="Gas Turbine"]
+        county_chp_gas_dict = county_chp_gas.loc[
+            :,('FIPS County', 'Capacity (kW)')].set_index('FIPS County')['Capacity (kW)'].to_dict()
+        
+        
+        
+        
+        #chp database for determining prime mover breakdown within naics subsector
+        chp_db_file = pd.read_csv('CHPDB_database.csv',thousands=',')
+        
+        chp_df = chp_db_file.loc[:,('Prime Mover','Capacity (kW)','NAICS')]
+        
+        chp_df = chp_df[(chp_df.NAICS >= 311111) & (chp_df.NAICS <= 339999)]
+        
+        
+        #consider only steam turbines and gas turbines, which make up 95% of capacity
+        chp_df = chp_df[(chp_df['Prime Mover'] == 'Boiler/Steam Turbine') | 
+                        (chp_df['Prime Mover'] == 'Combined Cycle') | 
+                        (chp_df['Prime Mover'] == 'Combustion Turbine')]
+        
+        chp_pm = chp_df.replace({'Prime Mover': 
+                                  {'Combined Cycle': 'Gas Turbine', 'Combustion Turbine': 'Gas Turbine'}})
+        
+        #determine prime mover breakdown, based on capacity, within naics subsector
+        
+        chp_pm['NAICS_sub'] = (chp_pm['NAICS'].astype(int)//1000).astype('str')
+        
+        chp_pm_grouped = chp_pm.groupby(
+            ['NAICS_sub','Prime Mover'])['Capacity (kW)'].sum()/chp_pm.groupby(['NAICS_sub'])['Capacity (kW)'].sum()
+        
+        chp_frac= chp_pm_grouped.reset_index()
+        
+        
+        chp_st = chp_frac[chp_frac['Prime Mover']=="Boiler/Steam Turbine"]
+        chp_st = chp_st.loc[:,('NAICS_sub', 'Capacity (kW)')]
+        chp_st_dict = chp_st.set_index('NAICS_sub')['Capacity (kW)'].to_dict()
+        chp_st_dict['336']=0
+        
+        chp_gas = chp_frac[chp_frac['Prime Mover']=="Gas Turbine"]
+        chp_gas = chp_gas.loc[:,('NAICS_sub', 'Capacity (kW)')]
+        chp_gas_dict = chp_gas.set_index('NAICS_sub')['Capacity (kW)'].to_dict()
+        
+        return chp_county_list, county_list_st, county_list_gas, county_list_both,                 county_chp_st_dict, county_chp_gas_dict, chp_st_dict, chp_gas_dict
+    
+        
+        
+        
+        
+
+
+# In[ ]:
+
+
+
+
