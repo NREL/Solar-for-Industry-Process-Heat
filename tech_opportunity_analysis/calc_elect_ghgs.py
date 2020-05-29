f4f895081d1796af1f2794ea4cce9cb02b762104
calmc <cmcmill@gmail.com>
2020-05-29 11:07:35 -0400

    Finished elect calcs

--- tech_opportunity_analysis/calc_elect_ghgs.py
+++ tech_opportunity_analysis/calc_elect_ghgs.py
@@ -12,15 +12,13 @@ import itertools
 import re
 import os
 
-class GHGs(object):
+class electricity(object):
     """
     Class containing methods used to calculate electricity emission factors
-    (MTCO2e/MMBtu) by county from EPA eGRID data.
+    (MTCO2e/MWh) by county from EPA eGRID data.
     """
 
-    data_wd = '../../Industry-Energy-Tool/data_for_calculations/ghgs/'
-
-    emission_factor_file = os.path.join(data_wd,'Emission factors.xlsx')
+    data_wd = './calculation_data'
 
     fips_to_zip_file = os.path.join(data_wd,'COUNTY_ZIP_032014.csv')
 
@@ -32,22 +30,24 @@ class GHGs(object):
         skiprows=[0, 1 ,2]
         )
 
-    zip_sub_region = pd.read_excel(e_grid_file, sheet_name='Zip-subregion')
-
-    # Should be downloaded as https://data.nrel.gov/files/97/County_industry_energy_use.gz
-    # from https://dx.doi.org/10.7799/1481899.
-    #county_energy_file = # See comments above
-
-    fuel_efs = pd.read_excel(
-        emission_factor_file, sheet_name='emission_factors', index_col=[0]
+    zip_sub_region = pd.concat(
+        [pd.read_excel(e_grid_file, sheet_name=sheet) for sheet in
+            ['Zip-subregion', 'Zip_multiple_Subregions']], axis=0,
+        ignore_index=True
         )
 
+    # Extracted from egrid summary tables for 2014 from
+    # https://www.epa.gov/sites/production/files/2020-01/egrid2018_historical_files_since_1996.zip
     resource_mix_file = os.path.join(data_wd, 'egrid2014_resource_mix.csv')
 
     resource_mix = pd.read_csv(resource_mix_file)
 
     @classmethod
     def calc_egrid_emissions_resource_mix(cls):
+        """
+        Estimate county grid emissions factor (metric tons CO2e per MWh),
+        resource mix, and losses as the mean of zip code egrid data.
+        """
 
         fips_zips = pd.read_csv(cls.fips_to_zip_file)
 
@@ -60,14 +60,14 @@ class GHGs(object):
                                       'state'], value_name='SUBRGN')
 
         subregions_ef_rm = pd.merge(subregions, cls.egrid_ef, on=['SUBRGN'],
-                                    how='left')
+                                    how='inner')
 
         subregions_ef_rm = pd.merge(subregions_ef_rm, cls.resource_mix,
                                     on=['SUBRGN'], how='left')
 
-        new_cols = set(['SRCO2RTA','SRCH4RTA','SRN2ORTA']).union(
-            cls.resource_mix.columns[1:]
-            )
+        new_cols = list(set(['SRCO2RTA','SRCH4RTA','SRN2ORTA']).union(
+            resource_mix.columns[1:]
+            ))
 
         subregions_ef_rm = subregions_ef_rm.groupby(
             ['ZIP (numeric)'], as_index=False
@@ -75,16 +75,20 @@ class GHGs(object):
 
         subregions_ef_rm.rename(columns={'ZIP (numeric)':'ZIP'}, inplace=True)
 
-        subregions_ef_rm.loc[:, 'MTCO2e_per_MMBtu'] = \
+        # Convert emissions fractors from lb/MWh to metric tons CO2e per MWh
+        # (MTCO2e/MWh)
+        subregions_ef_rm.loc[:, 'MTCO2e_per_MWh'] = \
             (subregions_ef_rm.SRCO2RTA + subregions_ef_rm.SRCH4RTA * 25 +
-             subregions_ef_rm.SRN2ORTA * 298) * (0.453592 / 3.412 / 10**3)
+             subregions_ef_rm.SRN2ORTA * 298) * (0.453592 / 10**3)
 
         subregions_ef_rm = subregions_ef_rm.set_index('ZIP').join(fips_zips,
                                                             how='left')
 
         county_ef_rm = subregions_ef_rm.reset_index().groupby(
             'COUNTY_FIPS', as_index=False
-            ).MTCO2e_per_MMBtu.mean()
+            )['grid_losses', 'MTCO2e_per_MWh', 'Natural_gas', 'Coal', 'Oil',
+              'Other_fossil','Solar', 'Biomass', 'Other', 'Hydro', 'Wind',
+              'Nuclear','Geothermal'].mean()
 
         county_ef_rm['COUNTY_FIPS'] = county_ef_rm.COUNTY_FIPS.astype('int')
 
