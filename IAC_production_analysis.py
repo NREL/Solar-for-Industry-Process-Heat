03881af725091c39d9a893d8d82b47abbb265d39
calmc <colin.mcmillan@nrel.gov>
2020-06-30 14:05:50 -0400

    Delete IAC_production_analysis.py

--- IAC_production_analysis.py
+++
@@ -1,6 +0,0 @@
-# download and unzip IAC database in memory
-# map SIC to NAICS
-# statistical analysis of operating hours by industry, and by year and industry
-# See if hours have changed
-# Look at summary stats for each industry
-# Create distributions for each industry 
