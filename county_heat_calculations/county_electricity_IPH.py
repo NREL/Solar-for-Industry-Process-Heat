80e6f30237f7e94bf7e832f462265d982f5e81e8
calmc <cmcmill@gmail.com>
2020-08-16 10:01:36 -0400

    Finished county electricity estimates
    Cleaned up county_electricity_IPH.py and added a copy of the resulting
    calculations to ../results/county_elec_estimates.csv.gzip. FYI
    @carriescho, @wxi1995.

--- county_heat_calculations/county_electricity_IPH.py
+++ county_heat_calculations/county_electricity_IPH.py
@@ -4,54 +4,6 @@ import Match_GHGRP_County_IPH as county_matching
 import get_cbp
 import Calculate_MfgEnergy_IPH
 
-# import mecs energy use, calculated from IPF algorithm. Keep only
-# electricity. Treat GHGRP establishments as large facilities, although a more
-# detailed analysis is possible using EIA-923 data.
-# mecs_elec = pd.read_csv(
-#     './calculation_data/mecs_2014_ipf_results_naics_employment.csv',
-#     index_col=0
-#     )
-# mecs_elec = mecs_elec[mecs_elec.MECS_FT == 'Net_electricity']
-# mecs_elec = mecs_elec.melt(
-#     id_vars=['MECS_Region', 'MECS_FT', 'Emp_Size'],
-#     var_name='MECS_NAICS_dummies', value_name='TBtu_per_est'
-#     )
-# mecs_elec.loc[:, 'MECS_NAICS_dummies'] = \
-#     mecs_elec.MECS_NAICS_dummies.astype(int)
-#
-
-####
-# Calculate intensities based on est counts from
-# '../results/mfg_energy_total_20200728-0804.parquet.gzip'
-##############
-
-
-#
-# mecs_intensities.set_index(
-#         ['MECS_Region', 'MECS_NAICS_dummies', 'Emp_Size'],
-#         inplace=True
-#         )
-#
-# county_est = pd.read_parquet(
-#     '../results/mfg_energy_total_20200728-0804.parquet.gzip',
-#     columns=['MECS_Region', 'COUNTY_FIPS', 'naics', 'MECS_NAICS', 'Emp_Size',
-#              'est_count'])
-#
-# cbp_grpd.set_index(
-#         ['MECS_Region', 'MECS_NAICS_dummies', 'Emp_Size'],
-#         inplace=True
-#         )
-#
-# mecs_intensities = mecs_intensities.join(cbp_grpd)
-#
-# mecs_intensities['intensity'] =\
-#     mecs_intensities.energy.divide(mecs_intensities.est_count,
-#                                    fill_value=0)
-#
-# mecs_intensities.drop(['energy', 'est_count'], axis=1, inplace=True)
-#
-# mecs_intensities.reset_index(inplace=True)
-
 # Code from run_all_IPH.py. Used to calculate MECS intensities based on
 # CBP establishment counts.
 energy_ghgrp = pd.read_parquet(
@@ -165,25 +117,7 @@ county_heat_est.loc[:, 'MMBtu'] = county_heat_est.est_count.multiply(
 county_elec = pd.concat([county_elec_mecs, county_elec_ghgrp, county_heat_est],
                         axis=0, ignore_index=True, sort=False)
 
-county_elec.to_csv('../results/county_elec_estimates.csv', index=False)
+county_elec = county_elec[county_elec.est_count > 0]
 
-# county_elec_ghgrp = pd.merge(
-#     county_est_ghgrp,
-#     mecs_intensities[mecs_intensities.Emp_Size == 'n1000'],
-#     on=['MECS_Region', 'MECS_NAICS'], how='left', suffixes=('', '_mecs')
-#     )
-# county_elec_ghgrp.loc[:, 'MMBtu'] = county_elec_ghgrp.est_count.multiply(
-#     county_elec_ghgrp.intensity
-#     )*10**6
-
-
-# Fill in NaN in est_count (corre)
-
-# Taking sum of establishments would double-count, as energy is disaggregated
-# by end use.
-county_est_size_mecs_region = county_energy.drop_duplicates(
-    ['MECS_Region', 'COUNTY_FIPS', 'naics', 'MECS_NAICS', 'Emp_Size'],
-    as_index
-    ).est_count.mean()
-
-# Estimate
+county_elec.to_csv('../results/county_elec_estimates.csv.gzip', index=False,
+                   compression='gzip')
