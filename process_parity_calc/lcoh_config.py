Bug Fixes in Process Parity Calculation
Fuel price was not being updated for both systems in iteration.
(1+corptax) in lcoh equation changed to (1-corptax) for LCOH systems
energy in replacement system was not being discounted -fixed

process_parity_calc/lcoh_config.py
@@ -25,7 +25,7 @@ class ParamMethods:
           "ptime": np.array([25]),
           "discount": {"BOILER": 0.0658, "CHP": 0.0658, "PVHP": 0.0658, "PTC" : 0.0658, 
                        "PTCTES" : 0.0658, "DSGLF" : 0.0658, "SWH": 0.0658, "PVEB" : 0.0658,
-                       "PVRH" : 0.0658, "FURNACE": 0.0658, "EBOILER": 0.0658},
+                       "PVRH" : 0.0723, "FURNACE": 0.0723, "EBOILER": 0.0658},
           "naics": 312120,
           "emp_size": "n1000",
           "op_hour": "avg",
@@ -95,7 +95,7 @@ class ParamMethods:
 
             return [np.array(ParamMethods.elec_rates[county]["touenergy"]), 
                     np.array([get_la(i.hour, i.dayofweek, i.month) * 0.13 for i in hourlystamp])]
-       
+        
         return [np.array(ParamMethods.elec_rates[county]["touenergy"]), np.array(ParamMethods.elec_curves[state_abbr])/1000]
     @classmethod 
     def get_demand_struc(cls,county):

process_parity_calc/models.py
@@ -33,6 +33,8 @@ class Tech(metaclass=ABCMeta):
 
         # Land Price
         self.state_name = mp.get_state_names(self.county)[0]
+        # burden rate
+        self.br = mp.get_burden_rate(self.state_name)
         
     @abstractmethod
     def om(self):
@@ -152,8 +154,6 @@ class Tech(metaclass=ABCMeta):
                     
             self.gen = self.load_8760/eff + kw_diff
 
-        # load not met - theoretical energy
-        self.load_remain = (np.array(self.load_8760) - self.gen*eff).clip(min=0)
         # excess load generation - actual energy
         self.elec_gen = (self.gen - np.array(self.load_8760)/eff).clip(min=0)
         # load met by solar - theoretical value
@@ -487,39 +487,32 @@ class DSGLF(Tech):
 
     def om(self):
         '''
-        All costs from SAM - dsg lf commercial owner
+        All costs from SAM
         
         ''' 
-        year = 2018
-        deflate_capital = [Tech.index_mult("Construction Labor", year), 
-                           Tech.index_mult("Solar Field", year),
-                           Tech.index_mult("Heat Exchangers and Tanks", year)]     
+        year = 2010
+        deflate_om = [Tech.index_mult("Engineering Supervision", year),
+                      Tech.index_mult("Construction Labor", year),
+                      Tech.index_mult("Engineering Supervision", year),
+                      Tech.index_mult("Engineering Supervision", year),
+                      Tech.index_mult("Construction Labor", year),
+                      Tech.index_mult("Construction Labor", year),
+                      Tech.index_mult("Solar Field", year),
+                      Tech.index_mult("Heat Exchangers and Tanks", year),
+                      Tech.index_mult("Heat Exchangers and Tanks", year)]
         
-        # capital costs
-        aperture = 3081.6
-        siteimprov = aperture * 20
-        solarfield = aperture * 150
-        HTF = aperture * 35
-        capex = [siteimprov,solarfield,HTF]    
-        indirect = 0.185
+        #perf_eng, plant equip ops, maint supervisor, TES maint supervisor, service cont-mirro wash, site maint, solar field maint, HTF maint, TES Maint
+        om_breakdown = np.array([63000*(1+self.br), 40000*(1+self.br), 48000*(1+self.br), 48000*(1+self.br), 350000, 70485, 755274, 231642, 427410])
+        cost_kw = om_breakdown/110000
         
-        self.om_val = 0.02* (1 + indirect) * sum(a * b for a, b in zip(deflate_capital, capex)) * self.mult
-        
-# =============================================================================
-#         deflate_price = [Tech.index_mult("Engineering Supervision", year),
-#                          Tech.index_mult("Engineering Supervision", year)]
-#         
-#         # fixed_om + var_om
-#         om = 55 * self.sys_size
-#         var_om = sum(self.gen)*0.004
-#         
-# 
-#         self.om_val = sum(a * b for a, b in zip(deflate_price, [om, var_om]))
-# =============================================================================
-        
+        om = cost_kw * self.sys_size
+
+        self.om_val = sum(a * b for a, b in zip(deflate_om, om)) * self.mult
+
         self.fc = 0
         self.decomm = 0
         self.em_costs = 0
+        self.elec_gen = np.array([0 for i in range(8760)])
     
     def capital(self):
         """
@@ -567,33 +560,23 @@ class PTC(Tech):
         All costs from SAM
         
         ''' 
-        year = 2018
-        deflate_capital = [Tech.index_mult("Construction Labor", year), 
-                           Tech.index_mult("Solar Field", year),
-                           Tech.index_mult("Heat Exchangers and Tanks", year)]      
+        year = 2010
+        deflate_om = [Tech.index_mult("Engineering Supervision", year),
+                      Tech.index_mult("Construction Labor", year),
+                      Tech.index_mult("Engineering Supervision", year),
+                      Tech.index_mult("Construction Labor", year),
+                      Tech.index_mult("Construction Labor", year),
+                      Tech.index_mult("Solar Field", year),
+                      Tech.index_mult("Heat Exchangers and Tanks", year)]
         
-        #m^2
-        aperture = 2624
-        siteimprov = aperture * 25
-        HTF = aperture*60
-        solarfield = aperture * 150
-        indirect = 0.185    
+        #perf_eng, plant equip ops, maint supervisor, service cont-mirro wash, site maint, solar field maint, HTF maint
+        om_breakdown = np.array([63000*(1+self.br), 40000*(1+self.br), 48000*(1+self.br), 350000, 70485, 755274, 231642])
+        cost_kw = om_breakdown/110000
         
-        capex = [siteimprov,HTF,solarfield]
+        om = cost_kw * self.sys_size
 
-        self.om_val = 0.02*(1 + indirect) * sum(a * b for a, b in zip(deflate_capital, capex)) * self.mult
-# =============================================================================
-#         deflate_price = [Tech.index_mult("Engineering Supervision", year),
-#                          Tech.index_mult("Engineering Supervision", year)]
-#         
-#         # fixed_om + var_om
-#         om = 66 * self.sys_size
-#         var_om = sum(self.gen)*0.004
-#         
-# 
-#         self.om_val = sum(a * b for a, b in zip(deflate_price, [om, var_om]))
-# =============================================================================
-        
+        self.om_val = sum(a * b for a, b in zip(deflate_om, om)) * self.mult
+
         self.fc = 0
         self.decomm = 0
         self.em_costs = 0
@@ -645,41 +628,29 @@ class PTCTES(Tech):
         All costs from SAM
         
         ''' 
-        year = 2018
+        year = 2010
+        deflate_om = [Tech.index_mult("Engineering Supervision", year),
+                      Tech.index_mult("Construction Labor", year),
+                      Tech.index_mult("Engineering Supervision", year),
+                      Tech.index_mult("Engineering Supervision", year),
+                      Tech.index_mult("Construction Labor", year),
+                      Tech.index_mult("Construction Labor", year),
+                      Tech.index_mult("Solar Field", year),
+                      Tech.index_mult("Heat Exchangers and Tanks", year),
+                      Tech.index_mult("Heat Exchangers and Tanks", year)]
+        
+        #perf_eng, plant equip ops, maint supervisor, TES maint supervisor, service cont-mirro wash, site maint, solar field maint, HTF maint, TES Maint
+        om_breakdown = np.array([63000*(1+self.br), 40000*(1+self.br), 48000*(1+self.br), 48000*(1+self.br), 350000, 70485, 755274, 231642, 427410])
+        cost_kw = om_breakdown/110000
+        
+        om = cost_kw * self.sys_size
 
-        deflate_capital = [Tech.index_mult("Construction Labor", year), 
-                           Tech.index_mult("Solar Field", year),
-                           Tech.index_mult("Heat Exchangers and Tanks", year),
-                           Tech.index_mult("Heat Exchangers and Tanks", year)]      
-        
-        # capital costs -> 560$/kW
-        aperture = 5248
-        siteimprov = aperture * 25
-        HTF = aperture*60
-        solarfield = aperture * 150
-        #62$/kwth
-        storage = 62 * 6000
-        indirect = 0.185
-        
-        capex = [siteimprov, HTF, solarfield, storage]
-        
-        # capital value
-        self.om_val = 0.02* (1 + indirect) * sum(a * b for a, b in zip(deflate_capital, capex)) * self.mult
-# =============================================================================
-#         deflate_price = [Tech.index_mult("Engineering Supervision", year),
-#                          Tech.index_mult("Engineering Supervision", year)]
-#         
-#         # fixed_om + var_om
-#         om = 66 * self.sys_size
-#         var_om = sum(self.gen)*0.004
-#         
-#         self.om_val = sum(a * b for a, b in zip(deflate_price, [om, var_om])) 
-# =============================================================================
-        
+        self.om_val = sum(a * b for a, b in zip(deflate_om, om)) * self.mult
+
         self.fc = 0
         self.decomm = 0
         self.em_costs = 0
-        self.elec_gen = np.array([0 for i in range(8760)])        
+        self.elec_gen = np.array([0 for i in range(8760)])
     
     def capital(self):
         """
@@ -801,9 +772,9 @@ class PVEB(Tech):
         if self.sys_size > 5000:
             om = self.sys_size * 1.2 * 14
 
-        #very low maintenance costs relative to boiler + assume same stationary engineer needed for boiler (0.23 burden rate)
+        #very low maintenance costs relative to boiler 
         #https://www.epsalesinc.com/electric-boilers-vs-gas-boilers/ says no annual maint cost- > 0.01 as an estimate to cover maint+maintlabor
-        om_eb = self.design_load * 61.02 * 0.01 + 62150 * 1.23 
+        om_eb = self.design_load * 61.02 * 0.01 #+ 62150 * 1.23 
         
         if self.storeval:
             om_batt = (self.storeval*335 + 4*292) * 0.025
@@ -1066,6 +1037,7 @@ class Boiler(Tech):
         self.tech_type = "BOILER"
         self.sys_size = self.peak_load
         self.load_met = np.array(self.load_8760)
+        self.capacity = mp.capacity
         
         # Boiler Sizing Function
         def select_boilers():
@@ -1146,24 +1118,6 @@ class Boiler(Tech):
             y = 11.534x5 - 34.632x4 + 39.736x3 - 21.955x2 + 6.2306x + 0.0967
         """
 
-
-# =============================================================================
-#         # Old Boiler Efficiency part load
-#         maxeff = self.eff_dict[self.fuel_type][1]
-#         #mineff = self.eff_dict[self.fuel_type][0]
-#         def boiler_eff(pload):
-#             c0 = 0.0967 
-#             c1 = 6.2306 * pload
-#             c2 = -21.955 * pload**2
-#             c3 = 39.736 * pload**3
-#             c4 = -34.632 * pload**4
-#             c5 = 11.534 * pload**5
-#             return sum([c0,c1,c2,c3,c4,c5])
-#         
-#         effmap = [min(maxeff * boiler_eff(load/self.peak_load) / 100,1)
-#                   for load in self.load_8760] 
-# =============================================================================
-        # jingyi Boiler Efficiency Part Load
         
         def boiler_eff(pload):
             return -0.1574 * pload**2 + 0.2697 * pload + 0.7019
@@ -1246,28 +1200,16 @@ class Boiler(Tech):
             return sum(a * b for a, b in zip(deflate_price, [u_c, a_d, d_l, s, m, r_p, o])) * count
 
         def om4(cap, shifts, hload, ash, HV = 28.608, count = 1):
-      
-# =============================================================================
-#             u_c = cap/0.45 * (580 * hload + 3900)
-#             d_l = (shifts*2190)/8760 * 105300 * 30.62/23.32
-# 
-#             if hload < 15:
-#                 s = (shifts*2190)/8760 * (hload - 5)/10 * 68500 * 30.62/23.32
-#                 m = (shifts*2190)/8760 * (1600 * hload + 8000) * 30.62/23.32
-#             else:
-#                 s = (shifts*2190)/8760 * 68500 * 30.62/23.32
-#                 m = (shifts*2190)/8760 * 32000 * 30.62/23.32
-#             r_p = 708.7 * hload + 4424 
-#             o = (0.3 * d_l + 0.26 * (d_l + s + r_p + m)) 
-# =============================================================================
+            shifts = self.capacity
             #use costs from cost breakdown instead
             u_c = 771.36*hload + 2101.6 #electricity + chemical costs
             #direct labor (how many shifts/year of work is being dedicated)
-            d_l = 62150 * (shifts*2190)/8760
+            d_l = 262800 * shifts * 0
             s = 0 #No supervision required
-            m = 16000/105300 * 62150  * (shifts*2190)/8760 
+            #maintenance is ~16% of direct labor costs usually
+            m = 16000/105300 * 262800  * shifts
             r_p = 708.72 * hload + 4424.3 #replacement parts
-            o = 0.23 *(d_l+m+s) #burden rate of 23% 
+            o = self.br *(d_l+m+s) #burden rate of 23% 
             
             #preserve maint/d_l cost when adjusting d_l
     
@@ -1466,7 +1408,7 @@ class Furnace(Tech):
         self.eff_dict = {"CRUCIBLE": 19, "REVERB": 40, "TOWER": 48}
         #theoretical energy to melt 1 ton of aluminum is 500 btu/lb or 293 kwh/ton
         # 8760 load profile must be actual energy delivered to end source (ie aluminum)
-        self.meltloss = {"CRUCIBLE": 3.5, "REVERB": 3, "TOWER": 2}
+        self.meltloss = {"CRUCIBLE": 3.5, "REVERB": 2, "TOWER": 2}
         # design load in ton/hr - energy efficiency doesn't affect steel production -> tons/hr steel
         if self.mult > 0:
             self.peak_load = self.mult
@@ -1507,7 +1449,7 @@ class Furnace(Tech):
         return effmap
     
 
-    def om(self,capacity = 1, shifts = 1, ash = 6):
+    def om(self,capacity = 1, shifts = 1):
 
         """ 
             Aluminum Furnace OM Costs

process_parity_calc/pr_par.py
@@ -20,19 +20,11 @@ class PrPar:
     
     def __init__(self, new_obj = True, form = False):
         """ solar_tech, comb_tech should be appropriate factory objects"""
-# =============================================================================
-#         county_list = [1133, 5149, 4027, 6115, 8125, 9015, 10005, 12133, 13321, 19197, 16087, 17203,
-#                        18183, 20209, 21239, 22127, 25027, 24510, 23031, 26165, 27173, 29510, 28163, 30111, 37199, 38105,
-#                        31185, 33019, 34041, 35061, 32510, 36123, 39175, 40153, 41071, 42133, 44009, 45091, 46137, 47189,
-#                        48507, 49057, 51840, 50027, 53077, 55141, 54109, 56045]
-# =============================================================================
 
-        #county_list = [6085, 6085, 6085, 6085, 6085, 6085]
-        #heat_list = [5000,10000, 15000, 20000, 25000, 30000]
-        solar_tech = ["PTCTES"]
-        comb_tech = ["BOILER"]
-        counties =["6037"]
- 
+        solar_tech = ["PTC", "PTC"]
+        comb_tech = ["BOILER", "BOILER"]
+        counties =["6037", "51095"]
+
 
         if new_obj:
             if form:
@@ -41,10 +33,10 @@ class PrPar:
                 
             else:
                 print("\nCreating solar object")
-                self.s_form = FormatMaker().create_format({"tech": solar_tech, "county":counties})
+                self.s_form = FormatMaker().create_format({"tech": solar_tech, "county": counties})
                     
                 print("\nCreating combustion object")  
-                self.c_form = FormatMaker().create_format({"tech": comb_tech, "county":counties})
+                self.c_form = FormatMaker().create_format({"tech": comb_tech, "county": counties})
                 
         self.solar = [CLO.LCOHFactory().create_LCOH(i) for i in self.s_form]
         self.comb = [CLO.LCOHFactory().create_LCOH(i) for i in self.c_form]
@@ -83,7 +75,7 @@ class PrPar:
             # Drop unnecessary columns
             df.drop(columns = ["Capital Cost", "Operating Cost"], inplace = True)
             # Grab default LCOH value
-            val_LCOH = df["LCOH Value US c/kwh"][0]
+            val_LCOH = df["LCOH Value US c/kwh"][1]
             # Find % change from default LCOH value
             df["LCOH Value US c/kwh"] = (df["LCOH Value US c/kwh"] - val_LCOH)/val_LCOH * 100
             #round % changes
@@ -105,22 +97,23 @@ class PrPar:
                 df.loc[no_vals*i+1:no_vals*i+no_vals, :] = copy.loc[no_vals*old_ind[i]+1:no_vals*old_ind[i]+no_vals,:].values
             
             # Beginning of tornado plot code
-            fig , ax = plt.subplots(length,1, figsize = (15,10))
-            title = fig.suptitle(techtype + " Tornado Diagram", fontsize = 30, fontweight="bold", x = 0.58)
+            fig , ax = plt.subplots(length,1, figsize = (15,10), dpi = 150)
+            title = fig.suptitle(techtype + " Replacement Tornado Diagram", fontsize = 30, fontweight="bold", x = 0.58)
     
             for i in range(length):
-                # Define masks for LCOH changes greater and less than 0
-                pmask = df.loc[no_vals*i+1:no_vals*i+no_vals, ["LCOH Value US c/kwh"]] > 0
-                nmask = df.loc[no_vals*i+1:no_vals*i+no_vals:, ["LCOH Value US c/kwh"]] < 0
-                # if both masks are empty, delete the axis/parameter since no sensitivity
-                if (not any(pmask["LCOH Value US c/kwh"])) and (not any(nmask["LCOH Value US c/kwh"])):
+                # Define masks for LCOH changes greater and less than tolerance
+                tolerance = 0.005
+                pmask = df.loc[no_vals*i+1:no_vals*i+no_vals, ["LCOH Value US c/kwh"]] > tolerance
+                nmask = df.loc[no_vals*i+1:no_vals*i+no_vals:, ["LCOH Value US c/kwh"]] < tolerance
+                # if any masks is empty, delete the axis/parameter since no sensitivity
+                if (not any(pmask["LCOH Value US c/kwh"])) or (not any(nmask["LCOH Value US c/kwh"])):
                     fig.delaxes(ax[i])
                     continue
 
                 sns.barplot(df[pmask]["LCOH Value US c/kwh"], ax = ax[i], color = "b", ci=None)
                 sns.barplot(df[nmask]["LCOH Value US c/kwh"], ax = ax[i], color = "r", ci=None)
                 ax[i].xaxis.set_visible(False)
-                ax[i].set_xlim([-120, 120])
+                ax[i].set_xlim([-60, 60])
                 [s.set_visible(False) for s in ax[i].spines.values()]
                 ax[i].set_yticklabels(columns[old_ind[i]], fontsize = 15, fontweight = "bold")
                 ax[i].yaxis.set_tick_params(length = 0)
@@ -172,7 +165,8 @@ class PrPar:
         elif iter_name.upper() == "FUELPRICE":
             for i in range(len(self.comb)):
                 self.comb[i].iter_name = "FUELPRICE"
-                root = bisection(lambda x: self.comb[i].iterLCOH(x) - self.solar_current[i], -50, 50, 100)
+                self.solar[i].iter_name = "FUELPRICE"
+                root = bisection(lambda x: self.comb[i].iterLCOH(x) - self.solar[i].iterLCOH(x), -50, 50, 100)
 
                 if root == None:
                     print("No solution")
@@ -193,22 +187,20 @@ class PrPar:
         no_plots = len(self.solar)
         """Solution Space"""
         for i in range(no_plots):
-            self.solar[i].iter_name = "INVESTMENT"
+            self.solar[i].iter_name = "BOTH"
         for i in range(no_plots):
             self.comb[i].iter_name = "FUELPRICE"
             
         #df_sol = pd.DataFrame(columns = ["fuelprice", "investment", "LCOH"])
         
-        i_vals = np.linspace(0 ,10 ** 7, no_val)
-        
         fig, ax = plt.subplots(no_plots, figsize = (10,15))
 
         # for each capital investment (i_vals) iterate to find associated fuel price
         for i in range(no_plots):
-            i_vals = np.linspace(0 ,10 ** 8, no_val)
+            i_vals = np.linspace(0 ,5*10**6, no_val)
             all_roots = []
             for j in i_vals:
-                root = bisection(lambda x: self.comb[i].iterLCOH(x) - self.solar[i].iterLCOH(j), -100, 100, 100)
+                root = bisection(lambda x: self.comb[i].iterLCOH(x) - self.solar[i].iterLCOH((x,j)), -100, 100, 100)
                 all_roots.append(root)
                 #df_sol = df_sol.append({"fuelprice": root, "investment" : i / 10**7}, ignore_index = True)
             try:
@@ -232,61 +224,70 @@ class PrPar:
         
         def get_pb(index):
             
-            # apply iter value
-# =============================================================================
-#             if iter_name == "FUELPRICE":
-#                 self.comb[index].fuel_price = iter_value
-#             elif iter_name == "INVESTMENT":
-#                 self.solar[index].iterLCOH("INVESTMENT", iter_value)
-#             else:
-#                 print("No such iteration parameter")
-# =============================================================================
+            # max year to check for payback period - else no point
+            ptime = [100]
+            # set period of analysis calculations
+            self.solar[index].p_time = ptime
+            self.comb[index].p_time = ptime
+            # calculate LCOH to get payback period variables
+            self.solar[index].calculate_LCOH()
+            self.comb[index].calculate_LCOH()
             
-            # Get the denominator of LCOH equation to calculate the numerator
-            energy_yield = sum([self.solar[index].load_avg * 31536000 / 
-                                (1 + self.solar[index].discount_rate[0]) ** i 
-                                for i in range(self.solar[index].p_time[0])])
-    
-            # numerator of the LCOH equation and first term (total solar cost discounted to year0)
-            value = list(-self.solar_current[index] * energy_yield / (3600*100))
-        
-            lifetime = self.solar[index].p_time[0]
+            i_solar = self.solar[index].year0[0]
+            a_solar = self.solar[index].cashflow
+            a_comb = self.comb[index].cashflow
+            savings = [i-j for i,j in zip(a_solar,a_comb)]
+            print(sum(savings), i_solar)
+            if sum(savings) + i_solar <=  0:
+                
+                year = 0
+                
+                while sum(savings[0:year+1]) + i_solar >= 0:
+                    year += 1
+                    
+                pb_year = year + (i_solar + sum(savings[0:year]))/(a_solar[year] - a_comb[year])
+                
+                #add year 0 solar cost to savings in year 0
+                savings[0] += -1 * i_solar
+                return (pb_year, savings[0:year+1])
+                
+            else:
+                savings[0] += -1 * i_solar               
+                return (False, savings[0:26])
             
-            # create the bar values for the cash flow diagram using combustion OM
-            for i in range(1, lifetime):
-                value.append(self.comb[index].OM(i)[0]/(1+self.solar[index].discount_rate[0])**i)
+        def get_irr(index):
+            
+            def irr_eq(dr):
+                # set period of analysis calculations
+                self.solar[index].discount_rate = dr
+                self.comb[index].discount_rate = dr
+                # calculate LCOH to get payback period variables
+                self.solar[index].calculate_LCOH()
+                self.comb[index].calculate_LCOH()
                 
-            pb_year = lifetime
-
-            # If total savings less than cost of solar system over lifetime
-            if np.sum(value[1:]) < -value[0]:
+                i_solar = self.solar[index].year0[0]
+                a_solar = self.solar[index].cashflow
+                a_comb = self.comb[index].cashflow
+                savings = [i-j for i,j in zip(a_solar,a_comb)]
                 
-                while (np.sum(value[1:]) < -value[0]) & (pb_year <= 149):
-                    value.append(self.comb[index].OM(pb_year)[0]/(1+self.solar[index].discount_rate[0])**pb_year)
-                    pb_year += 1
-        
-                if pb_year == 150:
-                    print("No Payback Year")                
-                
-            else: 
-                cash_flow = np.array(value[1:])
-                pb_year = np.argmax(cash_flow.cumsum() > -value[0])
-                
-            return pb_year, value
-        
+                return sum(savings) + i_solar    
+            
+            irr = bisection(lambda x: irr_eq([x]), 0 ,40, 100)
+            
+            return irr
             
         def plot_cf(index, value):  
             """ plot cash flow diagram"""
             t = np.arange(self.solar[index].year, self.solar[index].year + len(value), 1)
             
             fig, ax1 = plt.subplots(figsize=(12,6))
-            ax1.set_ylim([-5 * 10**7 , 10**7])
+            ax1.set_ylim([-2 * 10**6 , 3*10**5])
         
         
             title = fig.suptitle("Cash Flow Diagram", fontweight = "bold", fontsize = 20)
             ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
         
-            ax1.bar(t[:20], value[:20], color = ["#85bb65" if val >= 0 else "#D43E3E" for val in value], alpha = 0.8)
+            ax1.bar(t, value, color = ["#85bb65" if val >= 0 else "#D43E3E" for val in value], alpha = 0.8)
             ax1.set_xlabel("Year", fontsize = 15)
         
             ax1.set_ylabel('Annualized Cash Flow', color = "black", fontsize = 15)
@@ -306,8 +307,8 @@ class PrPar:
         
         for i in no_plots:
             payback, value = get_pb(i)
-            #payback, t, value, iter_value = iter_pb(i,iter_name, 15)
-            #print("The payback period is achieved within the lifetime of {} years when the {} is {}.".format(payback,iter_name,iter_value))
+            irr = get_irr(i)
+            print(payback, irr)
             plot_cf(i, value)
         
         self.reset()
@@ -317,22 +318,3 @@ if __name__ == "__main__":
     import pandas as pd
     test_obj = PrPar()
     print(test_obj.pp_1D("INVESTMENT"))
-
-# =============================================================================
-#     test_obj = PrPar()
-# 
-#     state_abbr =[]
-#     comb_lcoh = []
-#     solar_lcoh = []
-# 
-#     for i in range(len(test_obj.comb)):
-#         comb_lcoh.append(test_obj.comb[i].calculate_LCOH())
-#         solar_lcoh.append(test_obj.solar[i].calculate_LCOH())
-#         state_abbr.append(test_obj.comb[i].state_abbr)
-# 
-#     results = pd.DataFrame()
-#     results["STUSPS"] = state_abbr
-#     results["lcohdiff"] = [s/c for s,c in zip(solar_lcoh, comb_lcoh)]
-#     
-#     results.("state_lcoh_pveb.csv")
-# =============================================================================